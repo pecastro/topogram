f5dd9c03416d6b1b14cdcf3664fcfc6436b62b65
Paulo E. Castro <pauloedgarcastro@gmail.com>
2017-03-01 14:47:01 +0000

    add mobile config and generate app for android

---
+++ mobile-config.js
@@ -0,0 +1,14 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'com.example.pecastro.com',
+  name: 'topogram',
+  description: 'Topogram app',
+  author: 'Paulo Edgar Castro',
+  email: 'pauloedgarcastro@gmail.com',
+  website: 'http://wormholenet.com'
+});
+
+// Set PhoneGap/Cordova preferences
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
